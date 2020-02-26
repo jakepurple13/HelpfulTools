a1d7aa84715960821425cc49fe358f8dc9504013
jakepurple13 <jakerein@gmail.com>
2020-02-26 07:34:17 -0500

    -

--- app/src/main/java/com/programmersbox/helpfultools/MainActivity.kt
+++ app/src/main/java/com/programmersbox/helpfultools/MainActivity.kt
@@ -2,10 +2,12 @@ package com.programmersbox.helpfultools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v("asdf", "asdf")
     }
 }
