af920772fdd500b23015d2106324c3ce9e618b9f
jakepurple13 <jakerein@gmail.com>
2020-04-01 10:03:58 -0400

    -added scroll reached to test

--- testingplaygroundapp/src/main/java/com/programmersbox/testingplaygroundapp/MainActivity.kt
+++ testingplaygroundapp/src/main/java/com/programmersbox/testingplaygroundapp/MainActivity.kt
@@ -8,18 +8,46 @@ import androidx.recyclerview.widget.RecyclerView
 import com.programmersbox.dragswipe.Direction
 import com.programmersbox.dragswipe.DragSwipeAdapter
 import com.programmersbox.dragswipe.DragSwipeUtils
+import com.programmersbox.flowutils.RecyclerViewScroll
+import com.programmersbox.flowutils.scrollReached
+import com.programmersbox.helpfulutils.nextString
+import com.programmersbox.loggingutils.Loged
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.test_item.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testRV.adapter = CustomAdapter(mutableListOf("Hello", "World"))
+        Loged.FILTER_BY_PACKAGE_NAME = "programmersbox"
 
-        DragSwipeUtils.setDragSwipeUp(testRV.adapter as CustomAdapter, testRV, Direction.UP + Direction.DOWN, Direction.START + Direction.END)
+        val list = mutableListOf<String>().apply { repeat(20) { this += Random.nextString(5) } }
+
+        adapter = CustomAdapter(list)
+        testRV.adapter = adapter
+
+        DragSwipeUtils.setDragSwipeUp(adapter, testRV, Direction.UP + Direction.DOWN, Direction.START + Direction.END)
+
+        GlobalScope.launch {
+            testRV
+                .scrollReached()
+                .collect {
+                    runOnUiThread {
+                        when (it) {
+                            RecyclerViewScroll.START -> Loged.r("Start")
+                            RecyclerViewScroll.END -> adapter.addItem(Random.nextString(5))
+                        }
+                    }
+                }
+        }
     }
 
     inner class CustomAdapter(dataList: MutableList<String>) : DragSwipeAdapter<String, ViewHolder>(dataList) {
@@ -32,5 +60,4 @@ class MainActivity : AppCompatActivity() {
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
 }
