6afbf93d00745745d17a054c7af147f888b00f4f
jakepurple13 <jakerein@gmail.com>
2020-05-11 14:53:06 -0400

    -lowered min sdk

--- app/src/main/java/com/programmersbox/helpfultools/TestApp.kt
+++ app/src/main/java/com/programmersbox/helpfultools/TestApp.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import com.programmersbox.helpfulutils.createNotificationChannel
 import com.programmersbox.helpfulutils.createNotificationGroup
 import com.programmersbox.helpfulutils.defaultSharedPrefName
+import com.programmersbox.loggingutils.LogLevel
 import com.programmersbox.loggingutils.Loged
+import com.programmersbox.loggingutils.LogedInterceptor
 
 class TestApp : Application() {
 
@@ -12,9 +14,16 @@ class TestApp : Application() {
         super.onCreate()
         Loged.FILTER_BY_PACKAGE_NAME = "com.programmersbox.helpfultools"
         Loged.TAG = "HelpfulTools"
+        Loged.logedInterceptor = Interceptor()
         defaultSharedPrefName = "DefaultPrefNameTest"
         createNotificationChannel("testChannel")
         createNotificationGroup("testGroup")
     }
 
+}
+
+class Interceptor : LogedInterceptor {
+    override fun log(level: LogLevel, tag: String, msg: String) {
+        println("${level.name[0]}/$tag/$msg")
+    }
 }
