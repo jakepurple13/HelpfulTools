4899c3686ebbdf2e9544953015d1f26c8c7cc6b7
jakepurple13 <jakerein@gmail.com>
2020-07-10 12:19:14 -0400

    -added RetrofitUtils.kt!

--- thirdpartyutils/src/main/java/com/programmersbox/thirdpartyutils/PaletteUtils.kt
+++ thirdpartyutils/src/main/java/com/programmersbox/thirdpartyutils/PaletteUtils.kt
@@ -4,20 +4,19 @@ import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
-import androidx.core.graphics.applyCanvas
 import androidx.core.graphics.drawable.toBitmap
 import androidx.palette.graphics.Palette
 
 fun Drawable.getPalette() = Palette.from(toBitmap()).generate()
 fun Bitmap.getPalette() = Palette.from(this).generate()
 
-fun Int.getPaletteFromColor() = Bitmap.createBitmap(5, 5, Bitmap.Config.ARGB_8888)
+/*fun Int.getPaletteFromColor() = Bitmap.createBitmap(5, 5, Bitmap.Config.ARGB_8888)
     .applyCanvas { drawColor(this@getPaletteFromColor) }
     .let { Palette.from(it).generate() }
 
 fun String.getPaletteFromHexColor() = Bitmap.createBitmap(5, 5, Bitmap.Config.ARGB_8888)
     .applyCanvas { drawColor(Color.parseColor(this@getPaletteFromHexColor)) }
-    .let { Palette.from(it).generate() }
+    .let { Palette.from(it).generate() }*/
 
 fun Int.toColorDrawable() = ColorDrawable(this)
 fun String.toColorDrawable() = ColorDrawable(Color.parseColor(this))
