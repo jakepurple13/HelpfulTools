d9923a4a978b4545009700efa3f855cad0878f2d
jakepurple13 <jakerein@gmail.com>
2020-08-17 14:30:23 -0400

    - Kotlin 1.4!

--- helpfulutils/src/main/java/com/programmersbox/helpfulutils/NumberAndroidUtils.kt
+++ helpfulutils/src/main/java/com/programmersbox/helpfulutils/NumberAndroidUtils.kt
@@ -1,7 +1,11 @@
 package com.programmersbox.helpfulutils
 
 import android.graphics.Color
+import android.os.Handler
+import androidx.core.os.postDelayed
 
 fun Colors.ARGB.toColor() = Color.argb(a, r, g, b)
 fun Colors.RGB.toColor() = Color.rgb(r, g, b)
 fun Colors.CMYK.toColor() = toARGB().toColor()
+
+fun Number.wait(block: () -> Unit) = Handler().postDelayed(this.toLong(), action = block)
